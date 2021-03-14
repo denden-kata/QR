bc6df60428188573837a8a59da36b33eba0f3188
denden-kata <denden3104@gmail.com>
2021-03-14 23:40:50 +0900

    [update] パーミッションリクエストのダイアログを表示

--- app/src/main/java/com/example/qr/MainActivity.kt
+++ app/src/main/java/com/example/qr/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.qr
 
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val REQUEST_CODE: Int = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestPermission()
         checkPermission()
+    }
+
+    /**
+     * アプリに必要なパーミッションのリクエストを行う
+     */
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= 24) {
+            val permission: Array<String> = (
+                    arrayOf(android.Manifest.permission.CAMERA)
+                    )
+            ActivityCompat.requestPermissions(this, permission, REQUEST_CODE)
+        }
     }
 
     /**
