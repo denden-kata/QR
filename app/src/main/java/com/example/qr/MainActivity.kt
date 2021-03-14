08b874cf6fbd6de6d44bfce44a95f89c7f0a5ef5
denden-kata <denden3104@gmail.com>
2021-03-14 22:57:22 +0900

    [update] カメラのパーミッションが取れるか確認するテスト

--- app/src/main/java/com/example/qr/MainActivity.kt
+++ app/src/main/java/com/example/qr/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.qr
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkPermission()
     }
+
+    /**
+     * カメラのパーミッションが取れているか確認.
+     */
+    private fun checkPermission() {
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Log.d("checkPermission", "SUCCESS")
+        } else {
+            Log.d("checkPermission", "FAILED")
+        }
+    }
+
 }
